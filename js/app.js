3d696b4633fa81bf48a6b75769b36b663c7589a8
mizukifujikawa-ciccc <mz.fujikawa.ciccc@gmail.com>
2025-01-21 09:35:54 -0800

    exercise

--- js/app.js
+++ js/app.js
@@ -1,3 +1,49 @@
 class Calculator {
+    constructor() {
+        this.num1InputElement = document.querySelector('#num1');
+        this.num2InputElement = document.querySelector('#num2');
+        this.operationElement = document.querySelector('#operation');
+        this.calculateBtnElement = document.querySelector('#calculateBtn');
+        this.resultElement = document.querySelector('#result');
 
+        this.calculateBtnElement.addEventListener('click', () => this.compute());
+    }
+
+    add(num1, num2) {
+        this.result = Number(num1) + Number(num2);
+    }
+    subtract(num1, num2) {
+        this.result = num1 - num2;
+    }
+    multiply(num1, num2) {
+        this.result = num1 * num2;
+    }
+    divide(num1, num2) {
+        this.result = num1 / num2;
+    }
+    compute() {
+        const num1 = this.num1InputElement.value;
+        const num2 = this.num2InputElement.value;
+        const operation = this.operationElement.value;
+        if (operation === 'add') {
+            this.add(num1, num2);
+        }
+        else if (operation === 'subtract') {
+            this.subtract(num1, num2);
+        }
+        else if (operation === 'multiply') {
+            this.multiply(num1, num2);
+        }
+        else if (operation === 'divide') {
+            this.divide(num1, num2);
+        }
+        this.render();
+    }
+    render() {
+        this.resultElement.innerHTML = '';
+        this.resultElement.innerHTML = `${this.result}`
+    }
 }
+  document.addEventListener('DOMContentLoaded', () => {
+    const calculator = new Calculator()
+  })
